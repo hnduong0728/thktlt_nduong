74b96d30c41decbe77c61242b2ae0cab9fbfd303
hnduong0728 <huynhduong92nbk@gmail.com>
2025-12-05 21:39:14 +0700

    Update bai28.c

--- bai28.c
+++ bai28.c
@@ -24,19 +24,33 @@ int main() {
         double x;
         while (1) {
             scanf( "%lf", &x);
-            if ( x < 0 ) {
+            if ( x < 0 || x != (long long)x ) {
                 printf("Nhap lai so: %lf\n", x);
                 continue;
             }
-            a[i] = x;
+            a[i] = (long long)x;
             break;
         }
+    }
+
+    int all_zero = 1;
+    for ( i = 0 ; i < n ; i++ ) {
+        if (a[i] != 0) {
+            all_zero = 0;
+            break;
+        }
+    }
+
+    if ( all_zero ) {
+        printf("1 ");
+        printf("Khong ton tai bcnn");
+        return 0;
     }
 
     long long ketqua = a[0];
     long long ketqua1 = a[0];
 
-    for (int i = 1; i < n; i++) {
+    for ( i = 1; i < n; i++) {
         ketqua = ucln(ketqua, a[i]);
         ketqua1 = bcnn(ketqua1, a[i]);
     }
